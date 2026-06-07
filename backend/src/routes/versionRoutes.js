aeb4e9e5eda7667ff2b699e6b44c55d44b62ef31
bjrajafju <bjrajafju@gmail.com>
2026-06-07 20:15:42 +0100

    aaa

--- backend/src/routes/versionRoutes.js
+++ backend/src/routes/versionRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
-    latestVersion: "1.0.3",
-    minVersion: "1.0.3",
-    url: "http://localhost:3000/download/SearaSetup.exe"
+    latestVersion: "1.0.1",
+    minVersion: "1.0.0",
+    url: "https://seara.onrender.com/download/seara.exe"
   });
 });
 
